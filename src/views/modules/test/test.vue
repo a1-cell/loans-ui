98f730d63dda33f5ab725913e63ec20f18749d2b
minchangle <1404874558@qq.com>
2022-07-29 17:23:18 +0800

    E第三周

---
+++ src/views/modules/test/test.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+        <form method="post" action="http://localhost:9113/bha-neo-app/lanmaotech/service">
+            <input name="serviceName"  v-model="reqData.serviceName">
+            <input name="platformNo"  v-model="reqData.platformNo">
+            <input name="reqData"  v-model="reqData.reqData">
+            <input name="keySerial"  v-model="reqData.keySerial">
+            <input name="sign"  v-model="reqData.sign"><br>
+            <el-button type="primary" @click="toAibank">投标</el-button>
+        </form>
+    </div>
+</template>
+
+<script>
+    import axios from "axios";
+
+    export default {
+        name: "test",
+        data(){
+            return{
+                reqData:{
+                    serviceName:'',
+                    platformNo:'',
+                    reqData:{
+                        requestNo:'',
+                        platformUserNo:'',
+                        bizType:'',
+                        Amount:'',
+                        expired:''
+                    },
+                    keySerial:null,
+                    sign:''
+                }
+            }
+        },
+        methods:{
+            //开户
+            toAibank(){
+                document.forms[0].submit();
+            }
+        },
+        created() {
+            axios.post("http://localhost:9113/bha-neo-app/lanmaotech/initParam2").then((resp)=>{
+                console.log(resp)
+                this.reqData = resp.data;
+            });
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
